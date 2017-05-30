592d2c298309619e9d4c03345db1738e9472edc8
Sasha <sasha@babypterodactyl.com>
2017-05-30 00:14:43 -0500

    2nd commit

--- frontend-nanodegree-resume-master/js/resumeBuilder.js
+++ frontend-nanodegree-resume-master/js/resumeBuilder.js
@@ -5,34 +5,49 @@ This is empty on purpose! Your code to build the resume will go here.
 var bio = {
   name: "Baby Pterodactyl",
   role: "Front End Developer",
-  contactInfo: "sasha@babypterodactyl.com",
+  contactInfo: "email: sasha@babypterodactyl.com",
   picture: "https://cdn.pterodactyl.io/site-assets/logo-icon.png",
   welcomeMessage: "Hello, welcome to my page.",
-  skills: ["HTML", "CSS", "JavaScript"]
+  skills: ["HTML", "CSS", "JavaScript", "JQuery"]
 };
 
-var formattedBiopic = HTMLbioPic.replace("%data%",bio.picture);
-$("#header").prepend(formattedBiopic);
+var work = {
+  jobs: [
+    "Pancake Maker: 2009 - Present",
+    "Rare Book Evaluator: 2011 - 2012",
+    "Cat Meme Gatherer: 2016 - 2017"
+  ]
+}
 
-var formattedName = HTMLheaderName.replace("%data%", bio.name);
-$("#header").append(formattedName);
+var education = {}
 
-var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
-$("#header").append(formattedRole);
+// BIO OBJECT APPENDING BELOW //
 
-var formattedWelcomeMessage = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
-$("#header").append(formattedWelcomeMessage);
+var formattedHTMLbioPic = HTMLbioPic.replace("%data%",bio.picture);
+$("#header").prepend(formattedHTMLbioPic);
+
+var formattedHTMLheaderName = HTMLheaderName.replace("%data%", bio.name);
+$("#header").append(formattedHTMLheaderName);
+
+var formattedHTMLheaderRole = HTMLheaderRole.replace("%data%", bio.role);
+$("#header").append(formattedHTMLheaderRole);
+
+var formattedHTMLwelcomeMsg = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
+$("#header").append(formattedHTMLwelcomeMsg);
+
+//var formattedHTMLcontactGeneric = HTMLcontactGeneric.replace("%contact%", "Contact Me").replace("%data%",bio.contactInfo);
+//$("#header").append(formattedHTMLcontactGeneric);
+
+$("#header").append(HTMLcontactGeneric);
 
 
-$("#workExperience").append(HTMLskillsStart);
+$("#header").append(HTMLskillsStart);
 
-  for (i = 0; i <= bio.skills.length; i++) {
-    var formattedSkills = HTMLskills.replace("%data%",bio.skills[i]);
-  }
+for(i = 0; i < bio.skills.length; i++){
+  var formattedHTMLskills = HTMLskills.replace("%data%",bio.skills[i]);
+  $("#skills").append(formattedHTMLskills);
+}
 
-console.log(formattedSkills);
+// BIO OBJECT APPENDING END //
 
-
-
-var formattedContact = HTMLcontactGeneric.replace("%contact%", "Contact Me").replace("%data%",bio.contactInfo);
-$("#lets-connect").append(formattedContact);
+// WORK AND EDUCATION OBJECT APPENDING BELOW //
