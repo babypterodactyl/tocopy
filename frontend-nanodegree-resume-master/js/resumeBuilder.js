130414d2b73542112431e40c61a4f3bc06735d03
Sasha <sasha@babypterodactyl.com>
2017-05-31 16:42:51 -0500

    commit linted files

--- frontend-nanodegree-resume-master/js/resumeBuilder.js
+++ frontend-nanodegree-resume-master/js/resumeBuilder.js
@@ -5,49 +5,78 @@ This is empty on purpose! Your code to build the resume will go here.
 var bio = {
   name: "Baby Pterodactyl",
   role: "Front End Developer",
-  contactInfo: "email: sasha@babypterodactyl.com",
-  picture: "https://cdn.pterodactyl.io/site-assets/logo-icon.png",
+
+  contacts: {
+    mobile: "210-840-7607",
+    email: "sasha@babypterodactyl.com",
+    github: "https://github.com/babypterodactyl",
+    location: "Austin"
+  },
+
   welcomeMessage: "Hello, welcome to my page.",
-  skills: ["HTML", "CSS", "JavaScript", "JQuery"]
-};
+
+  skills: [
+    "HTML", "CSS", "JavaScript", "JQuery"
+  ],
+  bioPic: "https://cdn.pterodactyl.io/site-assets/logo-icon.png"
+}
+
+var education = {
+  schools: [{
+      name: "Udacity Nanodegree",
+      years: "2017 - 2017",
+      city: "Austin, TX",
+    },
+
+    {
+      name: "Viking Education Prep",
+      years: "2017 - 2017",
+      city: "Austin, TX"
+    }
+  ],
+
+  onlineCourses: [{
+      title: "Git and Github",
+      school: "Viking Code School",
+      dates: "2017",
+      url: "https://www.vikingcodeschool.com/prep"
+    },
+    {
+      title: "Introduction to Javascript",
+      school: "Team Treehouse",
+      dates: "2016 - 2017",
+      url: "https://teamtreehouse.com/home"
+    }
+  ]
+
+}
 
 var work = {
-  jobs: [
-    "Pancake Maker: 2009 - Present",
-    "Rare Book Evaluator: 2011 - 2012",
-    "Cat Meme Gatherer: 2016 - 2017"
+  jobs: [{
+      employer: "Dynamic Systems",
+      title: "Administrative Assistant - Billing",
+      dates: "2017 - 2017",
+      description: "Provide administrative and billing support to the projects team"
+    },
+    {
+      employer: "University of Texas at Austin",
+      title: "Procurement Specialist",
+      dates: "2017 - 2017",
+      description: "Departmental buyer for UT School of Social Work"
+    }
   ]
 }
 
-var education = {}
+var projects = {
+  projects: [{
+    title: "Sample Project 1",
+    dates: "2017",
+    description: "Enter description here",
+    images: [
+      "https://www.pets4homes.co.uk/images/breeds/13/large/09c3ab97c079eac13e829b44f457313e.jpg",
+      "https://imgix.ranker.com/user_node_img/50026/1000504467/original/baby-with-his-toy-photo-u1?w=650&q=50&fm=jpg",
+      "https://3eieb81408ox1bb9lj1pzgh7-wpengine.netdna-ssl.com/wp-content/uploads/2016/08/pug_puppy.jpg",
+    ]
 
-// BIO OBJECT APPENDING BELOW //
-
-var formattedHTMLbioPic = HTMLbioPic.replace("%data%",bio.picture);
-$("#header").prepend(formattedHTMLbioPic);
-
-var formattedHTMLheaderName = HTMLheaderName.replace("%data%", bio.name);
-$("#header").append(formattedHTMLheaderName);
-
-var formattedHTMLheaderRole = HTMLheaderRole.replace("%data%", bio.role);
-$("#header").append(formattedHTMLheaderRole);
-
-var formattedHTMLwelcomeMsg = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
-$("#header").append(formattedHTMLwelcomeMsg);
-
-//var formattedHTMLcontactGeneric = HTMLcontactGeneric.replace("%contact%", "Contact Me").replace("%data%",bio.contactInfo);
-//$("#header").append(formattedHTMLcontactGeneric);
-
-$("#header").append(HTMLcontactGeneric);
-
-
-$("#header").append(HTMLskillsStart);
-
-for(i = 0; i < bio.skills.length; i++){
-  var formattedHTMLskills = HTMLskills.replace("%data%",bio.skills[i]);
-  $("#skills").append(formattedHTMLskills);
+  }]
 }
-
-// BIO OBJECT APPENDING END //
-
-// WORK AND EDUCATION OBJECT APPENDING BELOW //
